10fd7eebac8da7ebd884d09c40344b5064117d88
Adamya-github <121171569+Adamya-github@users.noreply.github.com>
2024-08-31 17:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 app = Flask(__name__)
 
 # Load model and scaler
-model = joblib.load('D:\Machine learning\house_price_prediction\model\house_price_model.pkl')
-scaler = joblib.load('D:\Machine learning\house_price_prediction\model\scaler.pkl')
+model = joblib.load('/model/house_price_model.pkl')
+scaler = joblib.load('/model/scaler.pkl')
 
 @app.route('/')
 def index():
